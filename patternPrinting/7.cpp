c74423ed1cd5d058ac95647a2d2b6531ead690b6
SANJUG <sanjugsonowalofficial@gmail.com>
2026-09-04 23:16:32 +0530

    pattern programming with spaces

---
+++ patternPrinting/7.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int rows = 4;
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < i + 1; j++){
+            cout<<"  ";
+        }
+
+        for(int j = 0; j < rows - i; j++){
+            cout<<j + 1<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
