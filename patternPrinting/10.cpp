790aba6ead0211dfdf0b309d3b2dfaa158b12050
SANJUG <sanjugsonowalofficial@gmail.com>
2026-09-05 23:17:53 +0530

    pyramid pattern programming

---
+++ patternPrinting/10.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int rows = 4;
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < rows - i - 1; j++){
+            cout<<"  ";
+        }
+        for(int j = 0; j < 2 * i + 1; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
---
+++ patternPrinting/13a.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int rows = 4;
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < rows - i - 1; j++){
+            cout<<"  ";
+        }
+        
+        for (int j = 0; j < 2*i + 1; j++) {
+            int val = (j <= i) ? (j+1) : (2*i - j + 1);
+            cout << val << " ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
