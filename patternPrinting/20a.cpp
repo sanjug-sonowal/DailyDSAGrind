302e5ba3060012d89963c9af18005dc3c28a7a61
SANJUG <sanjugsonowalofficial@gmail.com>
2026-09-19 21:22:01 +0530

    add advance patterns hollo and noramal rhombus

---
+++ patternPrinting/20a.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int rows = 5;
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < rows; j++){
+            if(i == j || i + j == rows - 1){
+                cout<<"* ";
+            }else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
---
+++ patternPrinting/20c.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int rows = 5;
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < rows - i - 1; j++){
+            cout<<"  ";
+        }
+        for(int j = 0; j < rows; j++){
+            if(i == 0 || i == rows - 1 || j == 0 || j == rows - 1){
+                cout<<"* ";
+            }else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
+}
